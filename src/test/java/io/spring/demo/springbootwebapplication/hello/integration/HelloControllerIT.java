b3ac395700365f25161efc613a5e76b2d327000a
Mario Jonckers <mario_jonckers@peopleware.be>
2018-10-19 09:59:35 +0200

    Add Unit Tests

---
+++ src/test/java/io/spring/demo/springbootwebapplication/hello/integration/HelloControllerIT.java
@@ -0,0 +1,47 @@
+package io.spring.demo.springbootwebapplication.hello.integration;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+/*
+* The embedded server is started up on a random port by virtue of the
+* webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+* and the actual port is discovered at runtime with the @LocalServerPort.
+*/
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerIT {
+
+    @LocalServerPort
+    private int port;
+
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        this.base = new URL("http://localhost:" + port + "/");
+    }
+
+    @Test
+    public void getHello() {
+        ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+        assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
+    }
+}
